50306bbb6ac5352922c02645e5613a5d8fe3a512
NicoVnd <nicolas.vdm59@gmail.com>
2024-06-18 11:06:17 +0200

    fonctionnalite chat

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
-const io = socketIo(server);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -12,12 +11,18 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.broadcast.emit('notification', 'A user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        socket.broadcast.emit('notification', 'A user disconnected');
+    });
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
