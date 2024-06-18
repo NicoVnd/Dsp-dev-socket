08941ec3dc99879d05563cf045f41dbf1323d174
NicoVnd <nicolas.vdm59@gmail.com>
2024-06-18 14:58:54 +0200

    step 5

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(server);
-const path = require('path');
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/channel/:channelName', (req, res) => {
-    res.sendFile(__dirname + '/channel.html');
+app.get("/channel/:channelName", (req, res) => {
+  res.sendFile(__dirname + "/channel.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('join channel', (channel) => {
-        socket.join(channel);
-        socket.to(channel).emit('notification', `A user joined channel ${channel}`);
-    });
+  socket.on("join channel", (channel) => {
+    socket.join(channel);
+    socket.to(channel).emit("notification", `A user joined channel ${channel}`);
+  });
 
-    socket.on('chat message', (data) => {
-        io.to(data.channel).emit('chat message', data);
-    });
+  socket.on("chat message", (data) => {
+    io.to(data.channel).emit("chat message", data);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
