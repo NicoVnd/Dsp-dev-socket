88c246c5e98fc9216790a2ce09be425d57b6ab3e
NicoVnd <nicolas.vdm59@gmail.com>
2024-06-18 12:26:14 +0200

    channel

--- index.js
+++ index.js
@@ -4,22 +4,32 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/channel/:channelName', (req, res) => {
+    res.sendFile(__dirname + '/channel.html');
+});
+
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.broadcast.emit('notification', 'A user connected');
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        socket.broadcast.emit('notification', 'A user disconnected');
+    socket.on('join channel', (channel) => {
+        socket.join(channel);
+        socket.to(channel).emit('notification', `A user joined channel ${channel}`);
     });
 
     socket.on('chat message', (data) => {
-        io.emit('chat message', data);
+        io.to(data.channel).emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 });
 
